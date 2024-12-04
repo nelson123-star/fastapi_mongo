e3e67b20a8aae6fbdda299b1fdd1833457772445
Влад Логунов <vl3012@icloud.com>
2024-12-04 23:37:13 +0700

    commit 20241204 planner-sql

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
@@ -11,5 +12,9 @@ app = FastAPI()
 app.include_router(user_router, prefix = "/user")
 app.include_router(event_router, prefix="/event")
 
+@app.on_event("startup")
+def on_startup():
+    conn()
+
 # if __name__ == "main":
-#     uvicorn.run("main:app", host="0.0.0.0", port = 8080, reload = True)
+#     uvicorn.run("main:app", host="192.168.31.182", port = 8080, reload = True)
